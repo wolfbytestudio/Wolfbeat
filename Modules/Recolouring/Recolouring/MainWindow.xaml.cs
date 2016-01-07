621be69deadcfb52dab4fe87b6fdfccf6ae210ea
wolfbytestudio <wolfbytestudio@gmail.com>
2016-01-07 22:42:05 +0000

    Bug Fixes

--- Modules/Recolouring/Recolouring/MainWindow.xaml.cs
+++ Modules/Recolouring/Recolouring/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Drawing.Imaging;
 using System.Windows.Shapes;
 using System.Drawing;
+using System.IO;
+using System.Reflection;
 
 namespace Recolouring
 {
@@ -27,36 +29,56 @@ namespace Recolouring
             InitializeComponent();
         }
 
+        Bitmap bmp;
+
         private void button_Click(object sender, RoutedEventArgs e)
         {
+
             //Reference this code
             var cm = new ColorMatrix(new float[][]
             {
-              new float[] {1, 0, 0, 0, 0},
-              new float[] {0, 0, 0, 0, 0},
-              new float[] {0, 0, 0, 0, 0},
+              new float[] { byteToFloat((byte)sldRed.Value), 0, 0, 0, 0},
+              new float[] {0, byteToFloat((byte)sldGreen.Value), 0, 0, 0},
+              new float[] {0, 0, byteToFloat((byte)sldBlue.Value), 0, 0},
               new float[] {0, 0, 0, 1, 0},
               new float[] {0, 0, 0, 0, 1}
             });
 
-            var img = System.Drawing.Image.FromFile("C:\\img.png");
+            
+
+            var img = System.Drawing.Image.FromFile(@"C:\\img.png");
+
 
             var ia = new ImageAttributes();
             ia.SetColorMatrix(cm);
 
-            var bmp = new Bitmap(img.Width, img.Height);
+            bmp = new Bitmap(img.Width, img.Height);
             var gfx = Graphics.FromImage(bmp);
             var rect = new System.Drawing.Rectangle(0, 0, img.Width, img.Height);
 
             gfx.DrawImage(img, rect, 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
 
-            bmp.Save("D:\\processed.png", ImageFormat.Png);
+            ImageSourceConverter c = new ImageSourceConverter();
+            MemoryStream stream = new MemoryStream();
+            bmp.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
 
-            Uri uri = new Uri(@"D:\processed.png", UriKind.Absolute);
-            ImageSource imgSource = new BitmapImage(uri);
+            ImageSource newImage = (ImageSource)c.ConvertFrom(stream);
 
-            imgRecol.Source = imgSource;
+
+
+            imgRecol.Source = newImage;
 
         }
+
+        public static byte floatToByte(float f)
+        {
+            return (byte)(f * 255 % 256);
+        }
+
+        public static float byteToFloat(byte b)
+        {
+            return (float)(1.0F / 255) * b;
+        }
+
     }
 }
